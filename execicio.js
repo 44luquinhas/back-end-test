b1e3af9deadd91af029cc22950ee8ae354b3faee
44luquinhas <159542226+44luquinhas@users.noreply.github.com>
2025-04-03 09:58:52 -0300

    Add files via upload

---
+++ execicio.js
@@ -0,0 +1,63 @@
+// Exiba a mensagem "Olá, Mundo!" no console 
+
+console.log ("Olá pedro");
+
+//Crie uma variável com seu nome e exiba o valor no console
+
+let nome = "Lucas";
+
+// Some dois números e exiba o resultado.
+
+num01 = 500;
+num02 = 600;
+resultado = num01 + num02;
+
+console.log (resultado);
+
+//Subtraia dois números e exiba o resultado.
+
+num03 = 50;
+num04 = 25,
+resultado = num03 - num04;
+
+console.log (resultado);
+
+//Multiplique dois números e exiba o resultado.
+
+num05 = 50;
+num06 = 100;
+resultado = num05 * num06;
+
+console.log (resultado);
+
+//Divida dois números e exiba o resultado.
+
+num07 = 4000;
+num08 = 2000;
+resultado = num07 / num08;
+
+console.log (resultado);
+
+// Descubra o resto da divisão de um número por outro
+
+num07 = 10;
+num08 = 5;
+resultado = num07 % num08;
+
+console.log(resultado);
+
+// Crie uma variável booleana (true ou false) e exiba seu valor 
+
+let eu = true;
+
+
+
+
+
+
+
+
+
+
+
+
